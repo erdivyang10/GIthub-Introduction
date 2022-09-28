1a505b166e997075fa975118925c57102ee51118
Goswami Divyang <GoswamiDivyang@JohnDeere.com>
2022-09-28 12:10:21 +0530

    remoive file name

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ import pandas as pd
 def number_of_pages(filename):
 
         with pdfplumber.open(filename) as pdf:
-        #with pdfplumber.open("harness/_n4wl99z.pdf") as pdf:       
-                #data_table =  pdf.pages[2].extract_table()
+        
                 for tables in pdf.pages:
                         print('Pages in PDF files: ',tables)
 
